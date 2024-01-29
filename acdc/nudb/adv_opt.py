44c0388555ce762ceae6fbbacb5c15b8c23bcad5
Niels uit de Bos <nielius@gmail.com>
2024-01-29 12:11:37 -0800

    A few more experiments with my own file

--- acdc/nudb/adv_opt.py
+++ acdc/nudb/adv_opt.py
@@ -2,19 +2,43 @@ from dataclasses import dataclass
 from typing import Callable
 
 import torch
-from jaxtyping import Shaped, Float
+from jaxtyping import Shaped, Float, Num
+from torch._C import _get_cublas_allow_fp16_reduced_precision_reduction
+from transformer_lens import HookedTransformer
+from transformer_lens.hook_points import HookPoint
 
+from acdc.TLACDCCorrespondence import TLACDCCorrespondence
 from acdc.docstring.utils import AllDataThings
 from acdc.greaterthan.utils import get_greaterthan_true_edges, get_all_greaterthan_things
 from acdc.nudb.joblib_caching import joblib_memory
-from subnetwork_probing.transformer_lens.transformer_lens import HookedTransformer
+from acdc.tracr_task.utils import get_all_tracr_things, get_tracr_reverse_edges
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
 
 num_examples = 5
 metric = "kl_div"
 device = "cpu"
 
+
 @joblib_memory.cache
 def get_task_data() -> tuple[AllDataThings, dict]:
+    # batch size 6, seq len 4
+    task_data = get_all_tracr_things(task="reverse", metric_name="l2", num_examples=6, device=device)
+    true_edges = get_tracr_reverse_edges()
+
+    return task_data, true_edges
+
+
+# true edges is
+# dict[tuple[TargetHookName, TargetHookindex, SourceHookName, SourceHookIndex], bool]
+
+
+@joblib_memory.cache
+def get_task_data_greaterthan() -> tuple[AllDataThings, dict]:
     task_data = get_all_greaterthan_things(num_examples=num_examples, metric_name=metric, device=device)
     true_edges = get_greaterthan_true_edges(
         model=task_data.tl_model
@@ -22,23 +46,65 @@ def get_task_data() -> tuple[AllDataThings, dict]:
 
     return task_data, true_edges
 
+
 task_data, true_edges = get_task_data()
 
 
 # Goal: run it once, calculate the metric
 
+
 @dataclass
 class AblationRunner:
     model: HookedTransformer
+
+    def run_with_ablated_edges(
+        self,
+        edges_to_ablate: list,
+        input: Num[torch.Tensor, "batch seq"],
+        corrupted_input: Num[torch.Tensor, "batch seq"],
+    ):
+        # batch size 6, input data dim 4
+        output_original, _ = self.model.run_with_cache(input)
+        output_on_corrupted_input, corrupted_values_cache = self.model.run_with_cache(corrupted_input)
+
+        base_correspondence = TLACDCCorrespondence.setup_from_model(
+            self.model, True if self.model.cfg.positional_embedding_type == "standard" else NotImplementedError()
+        )
+
+        list(self.model.modules())
+        list(self.model.named_modules())
+        self.model.hook_dict
+
+        def test_hook(hook_point_out: torch.Tensor, hook: HookPoint) -> torch.Tensor:
+            print(123)
+            return hook_point_out + 1000
+
+        hook_name = "blocks.3.hook_resid_post"
+
+        output = self.model.run_with_hooks(input, fwd_hooks=[(hook_name, test_hook)])
+
+        print(123)
+        pass
 
 
 @dataclass
 class AdvOptExperiment:
     runner: AblationRunner
     metric: Callable[[torch.Tensor], torch.Tensor]
-    dataset: Shaped[torch.Tensor, "batch *data"]  # for greater than, it's int64; also see https://docs.kidger.site/jaxtyping/api/array/
+    dataset: Shaped[
+        torch.Tensor, "batch *data"
+    ]  # for greater than, it's int64; also see https://docs.kidger.site/jaxtyping/api/array/
+    corrupted_dataset: Shaped[torch.Tensor, "batch *data"]
 
-    def run(self) -> float:
+    def run_my_test(self) -> float:
+        logits: Float[torch.Tensor, "batch pos vocab"] = self.runner.run_with_ablated_edges(
+            [], input=self.dataset, corrupted_input=self.corrupted_dataset
+        )
+        cur_metric = self.metric(logits).item()
+
+        return cur_metric
+
+    def run_plain(self) -> float:
         logits: Float[torch.Tensor, "batch pos vocab"] = self.runner.model(self.dataset)
         cur_metric = self.metric(logits).item()
 
@@ -46,14 +112,12 @@ class AdvOptExperiment:
 
 
 experiment = AdvOptExperiment(
-    runner=AblationRunner(
-        model=task_data.tl_model
-    ),
+    runner=AblationRunner(model=task_data.tl_model),
     metric=task_data.validation_metric,
     dataset=task_data.test_data,
+    corrupted_dataset=task_data.test_patch_data,
 )
 
-experiment.run()
-
-
-
+print("wtf")
+metric = experiment.run_my_test()
+logger.info("Metric is %s", metric)
