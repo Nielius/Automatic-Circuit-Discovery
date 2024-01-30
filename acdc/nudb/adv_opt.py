d75475b27575633fae09659b64400cae82534454
Niels uit de Bos <nielius@gmail.com>
2024-01-29 18:53:43 -0800

    Simple wrapper around Thomas's SP

--- acdc/nudb/adv_opt.py
+++ acdc/nudb/adv_opt.py
@@ -8,7 +8,7 @@ from transformer_lens import HookedTransformer
 from transformer_lens.hook_points import HookPoint
 
 from acdc.TLACDCCorrespondence import TLACDCCorrespondence
-from acdc.TLACDCEdge import Edge
+from acdc.TLACDCEdge import EdgeWithInfo, Edge, IndexedHookPointName
 from acdc.acdc_graphics import graph_from_edges
 from acdc.docstring.utils import AllDataThings
 from acdc.greaterthan.utils import get_greaterthan_true_edges, get_all_greaterthan_things
@@ -16,6 +16,8 @@ from acdc.nudb.joblib_caching import joblib_memory
 from acdc.tracr_task.utils import get_all_tracr_things, get_tracr_reverse_edges
 
 import logging
+
+from subnetwork_probing.sp_utils import MaskedTransformer
 
 logger = logging.getLogger(__name__)
 
@@ -56,12 +58,65 @@ task_data, true_edges = get_task_data()
 
 
 @dataclass
+class CompleteMask:
+    # little bit like the TLACDCCorrespondence
+    values: dict[IndexedHookPointName, dict[IndexedHookPointName, float]]
+
+    @classmethod
+    def initialize_from_model(cls, model: HookedTransformer) -> "CompleteMask":
+        """Create a CompleteMask with all the maskable values set to 1.0.
+        The main point of this function is that it gives you a list of all
+        the edges that you can mask."""
+        pass
+
+    @classmethod
+    def _get_maskable_edges_from_model(cls, model: HookedTransformer) -> list[Edge]:
+        pass
+
+
+# TODO test:
+# 1. If you run the normal mask, you get precisely the same results
+# 2.
+
+
 class MaskedRunner:
+    masked_transformer: MaskedTransformer
+
+    def __init__(self, model: HookedTransformer):
+        self.masked_transformer = MaskedTransformer(model=model)
+        self.masked_transformer.freeze_weights()
+        self._freeze_all_masks()
+        self._set_all_masks_to_pos_infty()
+
+    def _freeze_all_masks(self):
+        """In the MaskedTransformer, every mask is a parameter. In this case, however,
+        we only want to run the model with fixed masks, so we freeze all the masks."""
+        for value in self.masked_transformer._mask_logits_dict.values():
+            value.requires_grad = False
+
+    def _set_all_masks_to_pos_infty(self):
+        for parameter in self.masked_transformer._mask_logits_dict.values():
+            parameter.data.fill_(float("inf"))
+
+    def run(
+        self, input: Num[torch.Tensor, "batch seq"], patch_input: Num[torch.Tensor, "batch seq"]
+    ) -> Num[torch.Tensor, "batch pos vocab"]:
+        with self.masked_transformer.with_fwd_hooks_and_new_cache(
+            ablation="resample", ablation_data=patch_input
+        ) as hooked_model:
+            return hooked_model(input)
+
+
+@dataclass
+class MaskedRunnerOld:
     model: HookedTransformer
+
+    def maskable_edges(self) -> list[Edge]:
+        pass
 
     def run_with_ablated_edges(
         self,
-        edges_to_ablate: list[Edge],
+        edges_to_ablate: list[EdgeWithInfo],
         input: Num[torch.Tensor, "batch seq"],
         corrupted_input: Num[torch.Tensor, "batch seq"],
     ):
@@ -71,21 +126,14 @@ class MaskedRunner:
 
         for edge in edges_to_ablate:
             # what do I need to do here?
+            # Depends on whether we are going to do the original ACDC approach, or the new SP approach.
             pass
-
-
-
-
-
-
 
         base_correspondence = TLACDCCorrespondence.setup_from_model(
             self.model, True if self.model.cfg.positional_embedding_type == "standard" else NotImplementedError()
         )
 
-
-
-        all_edges: list[Edge] = list(base_correspondence.edge_iterator())
+        all_edges: list[EdgeWithInfo] = list(base_correspondence.edge_iterator())
         all_edges_collection = base_correspondence.edge_dict()
         g = graph_from_edges(edge_collection=all_edges_collection, filename="nielstest.png", show_everything=True)
 
@@ -111,15 +159,10 @@ class AdvOptExperiment:
     corrupted_dataset: Shaped[torch.Tensor, "batch *data"]
 
     def run_my_test(self) -> float:
-        logits: Float[torch.Tensor, "batch pos vocab"] = self.runner.run_with_ablated_edges(
-            [], input=self.dataset, corrupted_input=self.corrupted_dataset
+        logits: Float[torch.Tensor, "batch pos vocab"] = self.runner.run(
+            input=self.dataset,
+            patch_input=self.corrupted_dataset,
         )
-        cur_metric = self.metric(logits).item()
-
-        return cur_metric
-
-    def run_plain(self) -> float:
-        logits: Float[torch.Tensor, "batch pos vocab"] = self.runner.model(self.dataset)
         cur_metric = self.metric(logits).item()
 
         return cur_metric
@@ -132,6 +175,5 @@ experiment = AdvOptExperiment(
     corrupted_dataset=task_data.test_patch_data,
 )
 
-print("wtf")
 metric = experiment.run_my_test()
 logger.info("Metric is %s", metric)
